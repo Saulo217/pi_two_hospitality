ce187136322dadf0f6fae9df8c9648a73ea805a9
Lauro <lauroliberatonneto@gmail.com>
2024-05-31 02:31:20 -0300

    1.1

--- controllers/equipeController.js
+++ controllers/equipeController.js
@@ -1,4 +1,63 @@
-// controllers/equipeController.js
-export const renderEquipePage = (req, res) => {
-    res.render("equipe");
-};
+
+import express from "express"
+const router = express.Router()
+import EquipeService from "../services/EquipeService.js";
+import Auth from "../middleware/Auth.js"
+
+
+
+// Rota para a pagina principal da equipe
+router.get("/equipe", Auth, (req,res) =>{
+    EquipeService.SelectAll().then((equipe)=> {
+        res.render("equipe", {
+            equipe : equipe
+        })
+    })
+})
+
+//Rota de inserção um novo membro a equipe
+router.post("/equipe/novo", Auth , (req,res) =>{
+    EquipeService.Create(
+        req.body.nome,
+        req.body.funcao,
+        req.body.pagamento,
+        req.body.encargo,
+    )
+   res.redirect("/equipe");
+})
+
+// Rota da pagina de Cadastro de um novo Membro
+router.all("/equipe/cadastro", Auth , (req,res) =>{
+    res.render("equipeCadastro")
+})
+
+//Rota para buscar um membro a ser editado
+router.get("/equipe/editar/:id", Auth, (req,res) =>{
+    const id = req.params.id
+    EquipeService.SelectOne(id).then((equipe) =>{
+        res.render("equipeEditar", {
+            equipe : equipe
+        })
+    })
+})
+
+//Rota para fazer a alteração de um Membro
+router.post("/equipe/update/:id", Auth, (req,res) => {
+    EquipeService.Update(
+        req.params.id,
+        req.body.nome,
+        req.body.funcao,
+        req.body.pagamento,
+        req.body.encargo,
+    )
+    res.redirect("/equipe")
+})
+
+//Rota de Exclusão de um membro
+router.get("/equipe/deletar/:id", Auth , (req,res) =>{
+    const id = req.params.id
+    EquipeService.Delete(id)
+    res.redirect("/equipe")
+})
+
+export default router
--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 import express from "express"; 
 import mongoose from "mongoose";
 import session from "express-session";
-import { renderEquipePage } from "./controllers/equipeController.js";
 import { renderIndexPage } from "./controllers/indexController.js";
 import { renderQuartosPage } from "./controllers/quartosController.js";
 import Auth from "./middleware/Auth.js";
+import equipeController from "./controllers/equipeController.js"
+
 
 const app = express(); 
 
@@ -26,11 +27,15 @@ mongoose.connect("mongodb://127.0.0.1:27017/loja");
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+
+app.use("/", equipeController)
+
+
+
 app.use("/", UsersController)
 
 app.get("/", Auth, renderIndexPage);
 
-app.get("/equipe", Auth, renderEquipePage);
 app.post('/createCliente', createCliente); // Rota para lidar com a criação de um novo cliente
 
 app.get("/quartos", Auth, renderQuartosPage);
