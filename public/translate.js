30fea2229d19a0a95ec4f4a24498d2c2f595aa1c
foguetedasociety <lucasalmeidabernardi@gmail.com>
2024-06-11 15:43:29 -0300

    v2.1

--- public/translate.js
+++ public/translate.js
@@ -1,17 +1,18 @@
 function index(title, buttons, placeholders, labels) {
   document.querySelector("title").innerText = title;
-  for (i = 0; i < labels.length; i++) {
+  for (i = 0; i < labels.length - 1; i++) {
     document.querySelectorAll("label")[i].innerText = labels[i];
   }
   for (i = 0; i < placeholders.length; i++) {
-    document.querySelectorAll("input")[i].setAttribute("placeholder", placeholders[i]);
+    document.querySelectorAll("input")[i]
+      .setAttribute("placeholder", placeholders[i]);
   }
   for (i = 0; i < buttons.length; i++) {
     document.querySelectorAll("button")[i].innerText = buttons[i];
   }
 }
 
-function equipe(ths, labels, placeholders, buttons) {
+function equipe(ths, labels, placeholders, buttons, title) {
   for (i = 0; i < ths.length; i++) {
     document.querySelectorAll("th")[i].innerText = ths[i];
   }
@@ -24,9 +25,11 @@ function equipe(ths, labels, placeholders, buttons) {
   for (i = 0; i < buttons.length; i++) {
     document.querySelectorAll("button")[i].innerText = buttons[i];
   }
+
+  document.querySelector('#equipe').innerText = title;
 }
 
-function quartos() {
+function quartos(ths, labels, placeholders, buttons, title) {
   for (i = 0; i < ths.length; i++) {
     document.querySelectorAll("th")[i].innerText = ths[i];
   }
@@ -40,9 +43,10 @@ function quartos() {
     document.querySelectorAll("button")[i].innerText = buttons[i];
   }
   for (i = 0; i < buttons.length; i++) {
-    document.querySelectorAll("options")[i].innerText = buttons[i];
+    document.querySelectorAll("option")[i].innerText = buttons[i];
   }
 
+  document.querySelector('#quartos').innerText = title;
 }
 
 function relatorio() {
@@ -50,7 +54,13 @@ function relatorio() {
     document.querySelectorAll("button.mes")[i].innerText = buttons[i];
   }
 }
+function navbar(navItem) {
+  for (i = 0; i < navItem.length; i++) {
+    document.querySelectorAll(".navItem")[i].innerText = navItem[i];
+  }
 
+
+}
 const pt = {
   quartos: () => {
     quartos(
@@ -63,14 +73,16 @@ const pt = {
         "Nome: ", "Tipo: ", "Camas: ", "Categoria: ",
         "Status: "
       ],
-      [ "Criar", "Cancelar" ],
+      ["Criar", "Cancelar"],
       [
         "Selecione o Status",
-        "Disponivel", 
+        "Disponivel",
         "Ocupado",
         "Manutenção"
-      ]
+      ],
+      'Quartos'
     )
+    navbar(['Clientes', 'Equipe', 'Quartos', 'Relatórios', 'Linguagem', 'Logout'])
   },
   relatorio: () => {
     relatorio(
@@ -91,6 +103,7 @@ const pt = {
         "Outubro", "Novembro", "Dezembro"
       ]
     )
+    navbar(['Clientes', 'Equipe', 'Quartos', 'Relatórios', 'Linguagem', 'Logout'])
   },
   equipe: function () {
     equipe(
@@ -112,8 +125,10 @@ const pt = {
       [
         "Cadastrar Membro",
         "Cancelar"
-      ]
+      ],
+      'Equipe'
     )
+    navbar(['Clientes', 'Equipe', 'Quartos', 'Relatórios', 'Linguagem', 'Logout'])
   },
   index: () => {
     index(
@@ -130,9 +145,9 @@ const pt = {
         "Digite a sua Cidade", "Digite o seu Estado", "Digite o seu CEP", "Digite o preço Diário"
       ],
     )
-  },
+    navbar(['Clientes', 'Equipe', 'Quartos', 'Relatórios', 'Linguagem', 'Logout'])
+  }
 }
-
 const en = {
   quartos: () => {
     quartos(
@@ -153,79 +168,85 @@ const en = {
         "Available",
         "Unavailable",
         "Maintenance"
+      ],
+      'Rooms'
+    )
+    navbar(['Clients', 'Team', 'Rooms', 'Sheets', 'Language', 'Logout'])
+  },
+  relatorio: () => {
+    relatorio(
+      [
+        [
+          "January", "February", "March",
+          "April", "May", "June",
+          "July", "August", "September",
+          "October", "November", "December",
+
+          "January", "February", "March",
+          "April", "May", "June",
+          "July", "August", "September",
+          "October", "November", "December",
+
+          "January", "February", "March",
+          "April", "May", "June",
+          "July", "August", "September",
+          "October", "November", "December"
+        ]
       ]
     )
+    navbar(['Clients', 'Team', 'Rooms', 'Sheets', 'Language', 'Logout'])
+  },
+  equipe: () => {
+    equipe(
+      [
+        "ID", "Name", "Role",
+        "Payday",
+        "Charge", "Edit",
+        "To remove"
+      ],
+      [
+        "Name: ", "Role: ", "Payment: ",
+        "Charge: "
+      ],
+      [
+        "Enter Name", "Enter Role",
+        "Enter payment day",
+        "Enter Charge"
+      ],
+      [
+        "Register Member",
+        "Cancel"
+      ],
+      'Team'
+    )
+    navbar(['Clients', 'Team', 'Rooms', 'Sheets', 'Language', 'Logout'])
+  },
+  index: () => {
+    index(
+      "Vale do Refúgio Hotel",
+      ["Select Rooms", "Register Client"],
+      [
+        "Full Name", "CPF", "Phone Number", "Email Address", "Address Details",
+        "Booking History", "Date Check-in", "Date Check-out",
+        "Daily Price (USD)", "Rooms"
+      ],
+      [
+        "Full Name", "Enter Your CPF", "Enter your Phone number",
+        "Enter your Email Address", "Enter your Street",
+        "Enter your Home number", "Enter your Neighborhood name",
+        "Enter your City name", "Enter your State/Province name", "Enter your Postal Code",
+        "Enter daily price"
+      ],
+    )
+    navbar(['Clients', 'Team', 'Rooms', 'Sheets', 'Language', 'Logout'])
   }
-    relatorio: () => {
-      relatorio(
-        [
-          [
-            "January", "February", "March",
-            "April", "May", "June",
-            "July", "August", "September",
-            "October", "November", "December",
-
-            "January", "February", "March",
-            "April", "May", "June",
-            "July", "August", "September",
-            "October", "November", "December",
-
-            "January", "February", "March",
-            "April", "May", "June",
-            "July", "August", "September",
-            "October", "November", "December"
-          ]
-        ]
-      )
-    },
-    equipe: () => {
-      equipe(
-        [
-          "ID", "Name", "Role",
-          "Payday",
-          "Charge", "Edit",
-          "To remove"
-        ],
-        [
-          "Name: ", "Role: ", "Payment: ",
-          "Charge: "
-        ],
-        [
-          "Enter Name", "Enter Role",
-          "Enter payment day",
-          "Enter Charge"
-        ],
-        [
-          "Register Member",
-          "Cancel"
-        ]
-      )
-    },
-    index: () => {
-      index(
-        "Vale do Refúgio Hotel",
-        ["Select Rooms", "Register Client"],
-        [
-          "Full Name", "CPF", "Phone Number", "Email Address", "Address Details",
-          "Booking History", "Date Check-in", "Date Check-out",
-          "Daily Price (USD)", "Rooms"
-        ],
-        [
-          "Full Name", "Enter Your CPF", "Enter your Phone number",
-          "Enter your Email Address", "Enter your Street",
-          "Enter your Home number", "Enter your Neighborhood name",
-          "Enter your City name", "Enter your State/Province name", "Enter your Postal Code",
-          "Enter daily price"
-        ],
-      )
-    }
-  }
-
-    async function changeLanguage(language = "pt", route = "") {
-    const langs = {
-        pt,
-  en
 }
 
-langs[language][route]()
+async function changeLanguage(language = "pt", route = "") {
+  const langs = {
+    pt,
+    en
+  }
+
+  langs[language][route]()
 }
